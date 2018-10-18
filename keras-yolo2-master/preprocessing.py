0d4d5fd7df62d8f5fce5a8b626c8372d76b3db64
Pandian <pandian-ra@users.noreply.github.com>
2018-10-18 16:51:16 -0700

    Update preprocessing.py

--- keras-yolo2-master/preprocessing.py
+++ keras-yolo2-master/preprocessing.py
@@ -8,6 +8,7 @@ from keras.utils import Sequence
 import xml.etree.ElementTree as ET
 from utils import BoundBox, bbox_iou
 
+
 def parse_annotation(ann_dir, img_dir, labels=[]):
     all_imgs = []
     seen_labels = {}
